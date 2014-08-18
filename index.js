aa07dc3bd5570f6a9e0ef1fdc5e1f41ecf664b9b
bredele <olivier.wietrich@gmail.com>
2014-08-17 20:15:56 -0600

    mockup basic leg

---
+++ index.js
@@ -0,0 +1,15 @@
+
+/**
+ * Module dependencies.
+ */
+
+var brick = require('brickjs');
+var tmpl = require('./template.html');
+
+
+/**
+ * Expose 'leg'
+ */
+
+module.exports = brick.extend(tmpl);
+
